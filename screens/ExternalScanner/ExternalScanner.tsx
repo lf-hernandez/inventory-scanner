73df472af1b449a0142f851fff7a7e9d63c86f74
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-10-05 22:18:09 -0400

    Refactor: WIP extract external scanner components.

--- screens/ExternalScanner/ExternalScanner.tsx
+++ screens/ExternalScanner/ExternalScanner.tsx
@@ -1,153 +1,39 @@
 import React, { FunctionComponent, useState } from 'react';
-import { Image, Button, TextInput, Text, StyleSheet, View, Modal, TouchableOpacity } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
+import { StyleSheet, View} from 'react-native';
+import QuantitySelector from '../../components/QuantitySelector';
+import BarcodeInput from '../../components/BarcodeInput';
+import NewItemModal from '../../components/NewItemModal';
 
 const ExternalScanner: FunctionComponent = () => {
     const [barcode, setBarcode] = useState('');
     const [quantity, setQuantity] = useState(1);
-    const [modalVisible, setModalVisible] = useState(false);
+    const [isModalVisible, setModalVisible] = useState(false);
     const [itemName, setItemName] = useState('');
     const [itemDescription, setItemDescription] = useState('');
-    const [selectedImage, setSelectedImage] = React.useState(null);
+    const [selectedImage, setSelectedImage] = useState(null);
 
-    const isDecButtonDisabled = () => quantity <= 1;
-
-    const incrementQuantity = () => setQuantity(quantity + 1);
-    const decrementQuantity = () => {
-        if (quantity > 1) {
-            setQuantity(quantity - 1);
-        }
-    };
-    const quantityInputHandler = (value: string) => {
-        let parsedValue = parseInt(value);
-        if (isNaN(parsedValue)) {
-            parsedValue = 1;
-        }
-        setQuantity(parsedValue);
-    };
-    const barcodeHandler = (value: string) => {
+    const barcodeHandler = () => {
         setModalVisible(true);
     };
 
-    const cancelSubmission = () => {
-        setModalVisible(false);
-    };
-
-    const submitNewItem = () => {
-        console.log('submission');
-    };
-    const itemNameInputHandler = (value: string) => {
-        setItemName(value);
-    };
-    const itemDescriptionHandler = (value: string) => {
-        setItemDescription(value);
-    };
-
-    let openImagePickerAsync = async () => {
-        let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
-
-        if (permissionResult.granted === false) {
-            alert('Permission to access camera roll is required!');
-            return;
-        }
-
-        let pickerResult = await ImagePicker.launchImageLibraryAsync();
-        console.log(pickerResult);
-
-        if (pickerResult.cancelled === true) {
-            return;
-        }
-
-        setSelectedImage({ localUri: pickerResult.uri });
-    };
-
-    const reset = async () => {
-        setSelectedImage(null);
-        await openImagePickerAsync();
-    };
-
-    if (selectedImage !== null) {
-        return (
-            <View style={styles.imgContainer}>
-                <Image
-                    source={{ uri: selectedImage.localUri }}
-                    style={styles.thumbnail}
-                />
-
-                <TouchableOpacity onPress={reset} style={styles.button}>
-                    <Text style={styles.buttonText}>Choose different photo</Text>
-                </TouchableOpacity>
-            </View>
-        );
-    }
-
     return (
         <View style={styles.container}>
-            <Modal
-                animationType="slide"
-                transparent={true}
-                visible={modalVisible}>
-                <View style={styles.modalContainer}>
-                    <View style={styles.modalView}>
-                        <Text>Add New Item</Text>
-                        <TextInput
-                            style={styles.input}
-                            onChangeText={itemNameInputHandler}
-                            value={itemName}
-                            placeholder="Item name">
-                        </TextInput>
-                        <TextInput
-                            style={styles.input}
-                            onChangeText={itemDescriptionHandler}
-                            value={itemDescription}
-                            placeholder="Item description"
-                        ></TextInput>
-
-                        <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
-                            <Text style={styles.buttonText}>Upload item photo</Text>
-                        </TouchableOpacity>
-
-                        <View style={styles.actionsContainer}>
-                            <Button
-                                onPress={cancelSubmission}
-                                title="Cancel"
-                            />
-                            <Button
-                                onPress={cancelSubmission}
-                                title="Submit"
-                            />
-                        </View>
-                    </View>
-                </View>
-            </Modal>
-
-            <View>
-                <TextInput
-                    value={barcode}
-                    onChangeText={barcodeHandler}
-                    style={styles.input}
-                    placeholder="Barcode"
-                    clearButtonMode="always" />
-            </View>
-
-            <View style={styles.quantitySelector}>
-                <Button
-                    onPress={decrementQuantity}
-                    title="-"
-                />
-                <TextInput
-                    style={styles.quantityInput}
-                    onChangeText={quantityInputHandler}
-                    value={quantity.toString()}
-                    keyboardType="number-pad"
-                    maxLength={4}
-                />
-                <Button
-                    onPress={incrementQuantity}
-                    title="+"
-                    disabled={isDecButtonDisabled()}
-                />
-            </View>
+            <NewItemModal
+                isModalVisible={isModalVisible}
+                itemName={itemName}
+                itemDescription={itemDescription}
+                setModalVisible={setModalVisible}
+                setItemDescription={setItemDescription}
+                setItemName={setItemName}
+            />
+            <BarcodeInput
+                barcode={barcode}
+                setBarcode={setBarcode}
+                barcodeHandler={barcodeHandler}/>
+            <QuantitySelector
+                quantity={quantity}
+                setQuantity={setQuantity}
+            />
         </View>
     );
 };
@@ -158,72 +44,6 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         alignItems: 'center',
         marginTop: 50
-    },
-    quantitySelector: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginTop: 50
-    },
-    input: {
-        width: 200,
-        padding: 10,
-        borderBottomColor: '#9E9E9E',
-        borderBottomWidth: 1,
-        color: 'black'
-    },
-    quantityInput: {
-        width: 50,
-        padding: 10,
-        borderBottomColor: '#9E9E9E',
-        borderBottomWidth: 1,
-        textAlign: 'center'
-    },
-    modalContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 22
-    },
-    modalView: {
-        margin: 20,
-        backgroundColor: '#E0E0E0',
-        borderRadius: 20,
-        padding: 35,
-        alignItems: 'center',
-        shadowColor: 'white',
-        shadowOffset: {
-            width: 0,
-            height: 2
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5
-    },
-    button: {
-        marginTop: 35,
-        backgroundColor: 'blue',
-        padding: 10,
-        borderRadius: 5
-    },
-    buttonText: {
-        fontSize: 20,
-        color: '#fff'
-    },
-    thumbnail: {
-        width: 300,
-        height: 300,
-        resizeMode: 'contain'
-    },
-    imgContainer: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    actionsContainer: {
-        flexDirection: 'row',
-        marginTop: 20
     }
 });
 
